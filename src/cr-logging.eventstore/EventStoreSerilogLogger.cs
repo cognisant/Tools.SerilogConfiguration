7c93f9c653ad744505e902644fc19d685a15c70c
Isaac Beizsley <IsaacBeizsley@hotmail.com>
2017-12-22 14:11:40 +0000

    Added EventStoreSerilogLogger
    This project currently uses EventStore.ClientApi.NetCore's RC; if/when .NetCore gets merged into the main package, we will need to change this

---
+++ src/cr-logging.eventstore/EventStoreSerilogLogger.cs
@@ -0,0 +1,25 @@
+﻿using EventStore.ClientAPI;
+using Serilog.Core;
+using System;
+
+namespace CR.Logging.EventStore
+{
+    public class EventStoreSerilogLogger : ILogger
+    {
+        private readonly Logger _logger;
+
+        public EventStoreSerilogLogger(Logger logger) => _logger = logger;
+
+        public void Error(string format, params object[] args) => _logger.Error(format, args);
+
+        public void Error(Exception ex, string format, params object[] args) => _logger.Error(ex, format, args);
+
+        public void Info(string format, params object[] args) => _logger.Information(format, args);
+
+        public void Info(Exception ex, string format, params object[] args) => _logger.Information(ex, format, args);
+
+        public void Debug(string format, params object[] args) => _logger.Debug(format, args);
+
+        public void Debug(Exception ex, string format, params object[] args) => _logger.Debug(ex, format, args);
+    }
+}
