06027a93aa5a3cc4545349b1b933f4c1a7662aac
Luke Parker <moongateuk@gmail.com>
2018-04-27 16:15:56 +0100

    Removed FAKE, updated build.cmd with cleaning code.

--- src/build.cmd
+++ src/build.cmd
@@ -1,8 +1,7 @@
 @echo off
 
-If NOT exist "./packages/FAKE/tools/Fake.exe" (
-".nuget/NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-)
+del dist /S /Q
+mkdir dist
 
 SET TARGET="Default"
 IF NOT [%1]==[] (set TARGET="%1")
@@ -12,9 +11,6 @@ IF NOT [%2]==[] (set BUILDMODE="%2")
 
 SET VERSION=0.0.0
 IF NOT [%3]==[] (set VERSION=%3)
-
-"packages/FAKE/tools/Fake.exe" build.fsx "target=%TARGET%" "buildMode=%BUILDMODE%" "version=%VERSION%"
-if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet pack -p:PackageVersion=%VERSION% cr-logging/cr-logging.csproj -o ../dist -c Release --include-symbols
 if %errorlevel% neq 0 exit /b %errorlevel%
