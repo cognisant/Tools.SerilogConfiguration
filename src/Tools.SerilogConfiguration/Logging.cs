8fbac4989600133df6d9e5ae6499d4a566ff2570
oscarhirst <10322447+oscarhirst@users.noreply.github.com>
2019-12-03 10:56:55 +0000

    CR -> CorSci codestyles

--- src/Tools.SerilogConfiguration/Logging.cs
+++ src/Tools.SerilogConfiguration/Logging.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="Logging.cs" company="Cognisant Research">
-// Copyright (c) Cognisant Research. All rights reserved.
+﻿// <copyright file="Logging.cs" company="Corsham Science">
+// Copyright (c) Corsham Science. All rights reserved.
 // </copyright>
 
 namespace CR.Tools.SerilogConfiguration
